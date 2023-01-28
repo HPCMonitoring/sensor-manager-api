fff8230d0473e595d170c568954adc9a0f8131d9
phucvinh57 <npvinh0507@gmail.com>
2023-01-28 18:14:44 +0700

    add custem error

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,12 @@
 import { prisma } from "@repositories";
+import { BadRequest } from "@exceptions";
 
 async function getById(userId: string) {
-    return prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
         where: { id: userId }
     });
+    if (user === null) throw new BadRequest("User not found !");
+    return user;
 }
 
 export const userService = {
