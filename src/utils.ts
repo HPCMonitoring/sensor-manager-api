acba27b74300c4a3a4c74a3e8111b3ae7d36a36e
nhancdt <nhancdt@vng.com.vn>
2023-03-29 17:48:47 +0700

    [improve] update logging config

--- src/utils.ts
+++ src/utils.ts
@@ -15,7 +15,7 @@ export function createPlugin(swaggerTags: HandlerTag[], routesOptions: RouteOpti
         });
     };
 }
-
+const LOG_DIR = "logs/";
 const myFormat = winston.format.printf(({ level, message, timestamp }) => {
     return `${timestamp} [${level}]: ${message}`;
 });
@@ -25,8 +25,9 @@ export const wlogger = winston.createLogger({
     format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), myFormat),
     defaultMeta: { service: "user-service" },
     transports: [
-        new winston.transports.File({ filename: "error.log", level: "error" }),
-        new winston.transports.File({ filename: "combined.log" })
+        new winston.transports.File({ filename: LOG_DIR + "error.log", level: "error", maxFiles: 5, maxsize: 10_000_000 }),
+        new winston.transports.File({ filename: LOG_DIR + "debug.log", level: "debug", maxFiles: 10, maxsize: 10_000_000 }),
+        new winston.transports.File({ filename: LOG_DIR + "trace.log", level: "silly", maxFiles: 30, maxsize: 10_000_000 })
     ]
 });
 
